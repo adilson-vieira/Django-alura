abc348da4c557fa2e984ca2a969a2092c8d4bb0b
adilson <adilson@linux-light.localdomain>
2020-08-02 19:27:07 -0300

    administração do Django

--- receitas/views.py
+++ receitas/views.py
@@ -5,20 +5,27 @@
 # def index(request):
 #    return HttpResponse('<h1>receitas</h1>')  
 
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from .models import Receita
 
 def index(request):
 
-    receitas = {
+    """ receitas = {
         1: 'Lasanha',
         2: 'Sopa de Legumes',
         3: 'Sorvete'
-    }
+    } """
+
+    receitas = Receita.objects.all() # busca as receitas no BD
 
     dados ={
-        'nome_das_receitas': receitas 
+        'receitas': receitas 
     }
     return render(request, 'index.html', dados)
 
-def receita(request):
-    return render(request, 'receita.html')
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id) # Model, primary key
+    receita_a_exibir = {
+        'receita': receita
+    }
+    return render(request, 'receita.html', receita_a_exibir)
